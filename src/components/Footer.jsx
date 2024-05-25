583459b6bdce7c15b2f014bbdad204b5475c4db8
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-05-25 10:35:00 +0530

    Add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="w-[100%] py-[1.4rem] flex justify-center items-center bg-[#292929] text-[#968e6f]">
       <div className="md:w-[40%] max-md:px-[1rem] flex justify-center items-center" style={{fontFamily:'Lora'}}>
         <p className="tracking-widest font-[400] text-[12px] uppercase text-center">
-          © 2024 NEOWEDS Photography | All Rights Reserved Developed by{" "}
+          © 2024 NEOWEDS Photography | All Rights Reserved Developed by <b>ShivaTech</b>
           
         </p>
       </div>
